fdfe455df26fb385995da9a849cb630aa3e4047d
oyaai <oyahidden@gmail.com>
2023-09-16 20:07:07 +0700

    Revert "modify layout comment hight in style sheet"
    This reverts commit f62345a43cf9d262823fa511011f79b9977cc5de.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import "./App.css";
 // import { Route, Routes } from "react-router-dom";
 // import HomePage from "./page/home";
 // import Event from "./page/event";
@@ -7,13 +7,13 @@ import Navbar from "./Components/Navbar/Navbar";
 // import Contact from "./page/contact";
 //  Import Components
 
-import About from "./Components/About/About";
-import Blog from "./Components/Blog/Blog";
-import Contact from "./Components/Contact/Contact";
-import Event from "./Components/Event/Event";
-import Footer from "./Components/Footer/Footer";
 import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Event from "./Components/Event/Event";
+import Blog from "./Components/Blog/Blog";
 import Staff from "./Components/Staff/Staff";
+import Footer from "./Components/Footer/Footer";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
